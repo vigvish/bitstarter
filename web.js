748930c559b340e6fbf9616ea5d61e86c17bf7b4
Vignesh Vishwanathan <vigvish@gmail.com>
2013-07-20 18:25:39 +0000

    changed the listening port

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	var fs = require ('fs');
-	var data = fs.readFileSync('index.html');
-	var buf = new Buffer (data);
-	response.send(buf.toString('utf-8'));
+app.get('/',function(request, response) {
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
